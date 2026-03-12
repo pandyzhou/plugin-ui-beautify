a21c2ccade1cd7141eff09822f5eb30888654ba2
pandyzhou <zhouyi8772@163.com>
2026-03-13 01:36:56 +0800

    v1.7.4: 强制 card-wrapper 子 div 继承深色背景

--- src/main/resources/console/main.js
+++ src/main/resources/console/main.js
@@ -4,7 +4,7 @@
 
   /* ========== CONSTANTS ========== */
   var PLUGIN_NAME = "plugin-ui-beautify";
-  var PLUGIN_VERSION = "1.7.3";
+  var PLUGIN_VERSION = "1.7.4";
   var LINK_ID = "ui-beautify-theme-css";
   var CANVAS_ID = "ui-beautify-fx-canvas";
   var CUSTOM_STYLE_ID = "ui-beautify-custom-css";
